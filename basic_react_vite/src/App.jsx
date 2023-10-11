8d338a71f00deaa5046b6e92644b05048032b8c7
varun vatsal <varunvatsal963@gmail.com>
2023-10-12 05:14:25 +0530

    setting up buttons and css

--- basic_react_vite/src/App.jsx
+++ basic_react_vite/src/App.jsx
@@ -5,10 +5,22 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  let counter = 5;
+
+  const addValue = ()=>{
+    console.log("value added", Math.floor(Math.random()*10) )
+    counter = counter + 1; 
+  }
+  const subValue = ()=>{
+    console.log("value subtracted", Math.floor(Math.random()*100))
+  }
 
   return (
     <>
-          <h1>hello world, vite</h1>
+          <h1>hello varun, vite</h1>
+          <h2>counter value: {counter}</h2>
+          <button id='increment' onClick={addValue}>increment</button>
+          <button id='decrement' onClick={subValue}>decrement</button>
     </>
   )
 }
