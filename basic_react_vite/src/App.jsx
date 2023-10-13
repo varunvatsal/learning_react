74f3a257144d84c6aa0438460d8200e0522b58b5
varun vatsal <varunvatsal963@gmail.com>
2023-10-13 06:02:23 +0530

    learning how to use props

--- basic_react_vite/src/App.jsx
+++ basic_react_vite/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card.jsx'
 
 function App() {
   let [counter, setCounter] = useState(0)
-  
+  let userName = {
+    name:'varun vatsal',
+    age: '20'
+  }
 
   const addValue = ()=>{
     if(counter<20){
@@ -17,7 +21,7 @@ function App() {
     }
   }
   const subValue = ()=>{
-    if(counter>0){
+    if(counter>-5){
       console.log(`previous counter value: ${counter}, new counter value: ${counter-1}`)
       setCounter(counter - 1)
     }
@@ -32,6 +36,7 @@ function App() {
           <h2>counter value: {counter}</h2>
           <button id='increment' onClick={addValue}>increment</button>
           <button id='decrement' onClick={subValue}>decrement</button>
+          <Card obj={userName}/>
     </>
   )
 }
---
+++ basic_react_vite/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Card(props){
+    return (
+        <h1>my name is {props.obj.name}</h1>
+    )
+}
+
+export default Card
