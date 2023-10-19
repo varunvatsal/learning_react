de73ad695944f9565e349f77543ff32a1ca7de32
varun vatsal <varunvatsal963@gmail.com>
2023-10-19 19:23:42 +0530

    added background image and added dropdown input select

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
+  let currency = 'usd'
+  let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+  useEffect(()=>{
+    fetch(url)
+    .then(res=>res.json())
+    .then((res)=>{setData(res[currency])})
+    console.log(data)
+  }
+  ,[currency])
 
   return (
     <>
       <div className='container'>
-        <div className='from'></div>
-        <div className='to'></div>
+        <div className='from'>
+          <input onChange={(e)=>{currency=e.target.value}} placeholder='from' className='from_input' />
+          <select name="cars" id="cars_from">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+          </select>
+
+        </div>
+        <div className='to'>
+          <input placeholder='to' className='to_input' readOnly/>
+          <select name="cars" id="cars_to">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+          </select>
+        </div>
       </div>
     </>
   )
